ea9e946cd0e74341eb8c52be0e5134870a4c9547
Shrikant Rajendra Patil <58933004+shrikant2753@users.noreply.github.com>
2023-08-04 17:47:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const element = document.getElementById("level");
+        let domLevel = 1;
+        
+        function countParentNodes(node) {
+            while (node.parentNode) {
+                node = node.parentNode;
+                domLevel++;
+            }
+        }
+        
+        countParentNodes(element);
+        alert("DOM level of id=level: " + domLevel);
